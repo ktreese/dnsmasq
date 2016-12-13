a177e23e9ab4c58bb97d63e9a5f01ac086b6f94e
Kris Reese <kris.reese@wwt.com>
2016-12-12 22:33:54 -0600

    Adds hosts unmanaged by Puppet

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,4 +33,21 @@ class dnsmasq {
 
   Host <<| tag == '3031' |>> ~> Service['dnsmasq']
 
+  # Add hosts that cannot have puppet agent
+  host { 'ts140.3031.net':
+    ensure       => present,
+    host_aliases => ['ts140'],
+    ip           => '192.168.1.2',
+    notify       => Service['dnsmasq'],
+    require      => Package['dnsmasq'],
+  }
+
+  host { 'qnreese01.3031.net':
+    ensure       => present,
+    host_aliases => ['qnreese01'],
+    ip           => '192.168.1.5',
+    notify       => Service['dnsmasq'],
+    require      => Package['dnsmasq'],
+  }
+
 }
